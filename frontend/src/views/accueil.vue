f73f9a2ea0e0ee468ea28664f5e91d866a501d78
almohelo <almoyner.heloise@gmail.com>
2021-02-07 22:50:19 +0100

    ajout bouton créer

--- frontend/src/views/accueil.vue
+++ frontend/src/views/accueil.vue
@@ -1,10 +1,13 @@
 <template>
-  <headerPage/>
+  <headerPage />
 
-<section class="sectionAccueil">
-  <articleAccueil/>
-  <top/>
-</section>
+    <section class="secCreer">
+      <button class="creer">Créer un message</button>
+    </section>
+  <section class="sectionAccueil">
+    <articleAccueil />
+    <top />
+  </section>
 
   <footer>
     <p>Groupomania 2021 - Tout droits réservés.</p>
@@ -17,17 +20,27 @@ import articleAccueil from "../components/articleAccueil.vue";
 import top from "../components/top.vue";
 export default {
   name: "accueil",
-  components:{
-     headerPage,
-     articleAccueil,
-     top
-  }
-}
+  components: {
+    headerPage,
+    articleAccueil,
+    top,
+  },
+};
 </script>
 
 <style lang="scss">
-.sectionAccueil{
+.secCreer{
+  border-bottom: 3px solid white;
+  padding-bottom: 20px;
+  width: 100%;
   display: flex;
+  justify-content: center;
+}
+.creer{
+  border: 1px solid red;
 }
 
+.sectionAccueil {
+  display: flex;
+}
 </style>
